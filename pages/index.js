725b637333a3be1bdf74847ca059bd482945c1cd
Narangarig Ganbold <narangarig@gmail.com>
2022-02-28 17:01:08 -0600

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+export default function Index() {
+  return (
+    <div className="h-screen w-full bg-black">
+      <img
+        src="/images/moon-sharks.jpg"
+        className="object-cover w-full h-full"
+        alt="moon sharks"
+      />
+    </div>
+  );
+}
