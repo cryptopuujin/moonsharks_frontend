f0670315be76c61697a9edb0af60813c1dc537e7
Narangarig Ganbold <narangarig@gmail.com>
2022-02-28 18:08:05 -0600

    update

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,21 @@
+import Head from "next/head";
+
 export default function Index() {
   return (
-    <div className="h-screen w-full bg-black">
-      <img
-        src="/images/moon-sharks.jpg"
-        className="object-cover w-full h-full"
-        alt="moon sharks"
-      />
-    </div>
+    <>
+      <Head>
+        <title>{`moonsharks.io`}</title>
+        <meta name="description" content="moonsharks.io" />
+        <link rel="icon" href="/favicon.png" />
+      </Head>
+      <div className="h-screen w-full bg-black">
+        {/* eslint-disable-next-line @next/next/no-img-element */}
+        <img
+          src="/images/moon-sharks.jpg"
+          className="object-cover w-full h-full"
+          alt="moon sharks"
+        />
+      </div>
+    </>
   );
 }
